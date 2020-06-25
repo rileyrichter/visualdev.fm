30e0f65fe3783beedb53a88e8d671024864d1183
rileyrichter <bparker@gmail.com>
2020-06-25 17:57:46 -0500

    Removed old files and cleaned up

--- main.js
+++
@@ -1,9 +0,0 @@
-import { CountUp } from 'https://rileyrichter.github.io/visualdev.fm/countUp.min.js';
-
-  var options = {
-	useEasing: true,
-  useGrouping: true,
-  seperator: ',',
-  decimal: '.',
-};
-var demo = new CountUp('number', 0, 4.25, 2, 2.5, options);
